86f04b01c34fd1b1eef0c438cb517d42e7565e04
azusa.hara.gw <azusa.hara.gw@nttdocomo.com>
2024-11-27 08:17:47 +0000

    日本語のローカライズ

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/city_list_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter RESAS App',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        ],
+      supportedLocales: [
+        Locale('ja')
+       ],
       home: CityListPage(),
     );
   }
