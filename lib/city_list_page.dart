0c329ae3014fc5a7623a5574f48a5c3645c9dff2
azusa.hara.gw <azusa.hara.gw@nttdocomo.com>
2024-11-28 12:55:18 +0000

    StatefulWidgetに変更（保存時に毎回インジケータが出ないようにした

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -2,10 +2,25 @@
 import 'package:flutter/material.dart';
 import 'city_detail_page.dart';
 
-class CityListPage extends StatelessWidget {
+class CityListPage extends StatefulWidget {
   const CityListPage({
     super.key,
   });
+
+  @override
+  State<CityListPage> createState() => _CityListPageState();
+}
+
+class _CityListPageState extends State<CityListPage> {
+  //lateは初期値を与えないときに使う
+  late Future<void> _future;
+
+  //画面を開いた時の処理
+  @override
+  void initState() {
+    super.initState();
+    _future = Future.delayed(const Duration(seconds: 3));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,33 +42,32 @@ class CityListPage extends StatelessWidget {
         title: const Text('市区町村一覧'),
       ),
       body: FutureBuilder<void>(
-        future: Future.delayed(const Duration(seconds: 3)),
-        //３秒間の中の処理、builder
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
+          future: _future,
+          //３秒間の中の処理、builder
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView(
+              children: [
+                for (final city in cities)
+                  ListTile(
+                    title: Text(city),
+                    subtitle: const Text('政令指定都市'),
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(city: city),
+                        ),
+                      );
+                    },
+                  ),
+              ],
             );
-          }
-          return ListView(
-            children: [
-              for(final city in cities)
-                ListTile(
-                  title: Text(city),
-                  subtitle: const Text('政令指定都市'),
-                  trailing: const Icon(Icons.navigate_next),
-                  onTap: () {
-                    Navigator.of(context).push<void>(
-                    MaterialPageRoute(
-                    builder: (context) => CityDetailPage(city: city),
-                    ),
-                    );
-                 },
-                ),
-            ],
-          );
-        }
-      ),
+          }),
     );
   }
 }
